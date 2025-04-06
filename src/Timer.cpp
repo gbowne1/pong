7cc06895b468216f6ae7afba255c727c494c0681
Gregory Bowne <gbowne144@gmail.com>
2025-04-05 21:13:07 -0700

    updating build

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -4,39 +4,47 @@
 #include <thread>
 #include <future> // Ensure future is included
 
-namespace pong {
+namespace pong
+{
 
-void Timer::delay(const std::chrono::milliseconds& ms) {
-    std::unique_lock<std::mutex> lck(mtx);
-    _completed = false;
-    _isRunning = true;
-    lck.unlock();
-    
-    auto time_started = std::chrono::steady_clock::now();
-    std::this_thread::sleep_for(ms);
+    void Timer::delay(const std::chrono::milliseconds &ms)
+    {
+        std::unique_lock<std::mutex> lck(mtx);
+        _completed = false;
+        _isRunning = true;
+        lck.unlock();
 
-    lck.lock();
-    _isRunning = false;
-    _completed = true;
-}
+        auto time_started = std::chrono::steady_clock::now();
+        std::this_thread::sleep_for(ms);
 
-bool Timer::isRunning() const {
-    std::unique_lock<std::mutex> lck(mtx);
-    return _isRunning;
-}
-
-bool Timer::isCompleted() const {
-    std::unique_lock<std::mutex> lck(mtx);
-    return _completed;
-}
-
-bool Timer::start(const std::chrono::milliseconds& ms) {
-    if (isRunning()) {
-        return false;
-    } else {
-        _ftr = std::async(std::launch::async, &Timer::delay, this, ms);
-        return true;
+        lck.lock();
+        _isRunning = false;
+        _completed = true;
     }
-}
+
+    bool Timer::isRunning() const
+    {
+        std::unique_lock<std::mutex> lck(mtx);
+        return _isRunning;
+    }
+
+    bool Timer::isCompleted() const
+    {
+        std::unique_lock<std::mutex> lck(mtx);
+        return _completed;
+    }
+
+    bool Timer::start(const std::chrono::milliseconds &ms)
+    {
+        if (isRunning())
+        {
+            return false;
+        }
+        else
+        {
+            _ftr = std::async(std::launch::async, &Timer::delay, this, ms);
+            return true;
+        }
+    }
 
 } // namespace pong
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "Game.h"
+#include "../include/Game.h"
 #include <iostream>
 
 int main(int argc, char *argv[])
